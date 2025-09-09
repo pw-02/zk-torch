e9f978e5c95e86f441c4ec9e5c13690e1de85119
pw-02 <watters.patrick@outlook.com>
2025-09-08 22:38:35 -0700

    ert_fixed.onnx

---
+++ remove_dynamic_imports.py
@@ -0,0 +1,16 @@
+import onnx
+
+# Load model
+model = onnx.load("buildmodels/bert.onnx")
+
+# Edit input shapes
+for input_tensor in model.graph.input:
+    for dim in input_tensor.type.tensor_type.shape.dim:
+        if dim.dim_param:  # dynamic dimension like "batch_size"
+            if dim.dim_param == "batch_size":
+                dim.dim_value = 1
+            elif dim.dim_param == "sequence":
+                dim.dim_value = 384
+
+# Save back
+onnx.save(model, "buildmodels/bert_fixed.onnx")
