f1bfbf9df86b29400c2068c09dad6a362796dfca
pw-02 <watters.patrick@outlook.com>
2025-09-08 23:32:59 -0700

    c1

--- buildmodels/bert.py
+++ buildmodels/bert.py
@@ -122,15 +122,15 @@ def save_to_onnx(model, output_path):
         output_names=["output_start_logits", "output_end_logits"],
         opset_version=14,
         # do_constant_folding=True,
-        dynamic_axes=(
-            {
-                "input_ids": {0: "batch_size"},
-                "input_mask": {0: "batch_size"},
-                "segment_ids": {0: "batch_size"},
-                "output_start_logits": {0: "batch_size"},
-                "output_end_logits": {0: "batch_size"},
-            }
-        ),
+        # dynamic_axes=(
+        #     {
+        #         "input_ids": {0: "batch_size"},
+        #         "input_mask": {0: "batch_size"},
+        #         "segment_ids": {0: "batch_size"},
+        #         "output_start_logits": {0: "batch_size"},
+        #         "output_end_logits": {0: "batch_size"},
+        #     }
+        # ),
     )
 
 def main():
