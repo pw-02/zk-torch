aad5f9bab6693c096119e6f9e1dbd386a2a7fe33
pw-02 <watters.patrick@outlook.com>
2025-09-08 23:43:48 -0700

    nano_gpt_4_layers_64_embd

--- buildmodels/gpt/clean_gpt.py
+++ buildmodels/gpt/clean_gpt.py
@@ -4,7 +4,7 @@ from replace_gelu import replace_gelu
 from replace_rope import replace_for_rope, replace_for_rope_mul
 from replace_multihead import replace_for_multihead
 
-model_path = 'buildmodels/gpt/nano_gpt_2_layers_8_embd.onnx'
+model_path = 'buildmodels/gpt/nano_gpt_4_layers_64_embd.onnx'
 
 model = onnx.load(model_path, load_external_data=False)
 
