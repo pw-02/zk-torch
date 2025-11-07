ed53efed7fe7ac252a7ad501a222bdb026d58b85
pw-02 <watters.patrick@outlook.com>
2025-11-06 17:17:28 -0800

    path update

--- tinybertmodels/gen_yamls.py
+++ tinybertmodels/gen_yamls.py
@@ -17,7 +17,7 @@ for file in os.listdir(models_dir):
         yaml_content = {
             "task": model_name,
             "onnx": {
-                "model_path": os.path.join("buildmodels", "bert", file),
+                "model_path": f"buildmodels/bert/{file}",
                 # model_path: bert_fixed.onnx --- IGNORE ---
                 "input_path": ""
             },
